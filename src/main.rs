e672853579ae7eaf0d54fe6d289d72714f5b251b
leuction <leuction@gmail.com>
2019-10-16 14:43:21 -0400

    added the main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate clap;
+use clap::{App, Arg, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+  let matches = App::new("My Super Program")
+    .version("1.0")
+    .author("Kevin K. <kbknapp@gmail.com>")
+    .about("Does awesome things")
+    .arg(
+      Arg::with_name("INPUT")
+        .help("Sets the input file to use")
+        .required(true)
+        .index(1),
+    )
+    .get_matches();
+
+  // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
+  // required we could have used an 'if let' to conditionally get the value)
+  println!("Using input file: {}", matches.value_of("INPUT").unwrap());
 }
